b4a6e1babae6631987ec81ba90772eefab66058a
Tom Yu <tomyu@TomdeMacBook-Pro.local>
2019-07-12 01:05:05 +0800

    optimize compile file construct

--- webpack.x.error.js
+++ webpack.x.error.js
@@ -6,13 +6,29 @@
 
 {
   
-
+ 1
   `
+  ArgumentError: HappyThreadPool requires a positive integer for its size , but got {0}
+
   Reason: macOs线程池不够，估计cpu核数不够
   Method：改为开启所有线程，const HappyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length - 1 }); 
-  Logs:
-  ArgumentError: HappyThreadPool requires a positive integer for its size , but got {0}
-  
+  `
+}
+
+{
+  2
+  `
+  Error: Command failed: open -a "Chrome" "http://localhost:8088"
+  Unable to find application named 'Chrome'
+
+  Reason：mac下chrome叫 Google Chrome 
+
+  open-browser-webpack-plugin需要判断系统
+  new OpenBrowserWebpackPlugin({
+    browser: os.type()!=='Darwin' ?'Google Chrome':'Chrome',
+    url: 'http://localhost:8088',
+  }),
+
   `
 }
 
