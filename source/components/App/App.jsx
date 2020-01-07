776497590953bbf2c923f0c7132d8d95372310df
tomy <aladingding@live.com>
2020-01-07 14:48:38 +0800

    3132

--- source/components/App/App.jsx
+++ source/components/App/App.jsx
@@ -14,7 +14,13 @@ import ajax from '@/components/Api/ajax.js';
 const fetch = require('node-fetch');
 import {PhotoSwipe} from 'react-photoswipe';
 import 'react-photoswipe/lib/photoswipe.css';
+import ImageLoader from 'react-imageloader';
 
+import LazyLoad from 'react-lazyload';
+const loadingImg = require('./giphy.gif');
+function preloader() {
+    return <img src={loadingImg} />;
+}
 class App extends React.Component {
     constructor(props){
         super(props)
@@ -45,7 +51,7 @@ class App extends React.Component {
         
         setTimeout(()=>{
             this.setState({
-                openPreview: true
+                openPreview: false
             })
         },3000)
     }
@@ -72,17 +78,70 @@ class App extends React.Component {
             editorState, outputHTML,openPreview,previewList
         } = this.state;
 
+        const imgList = [
+            'http://img.kuai8.com/attaches/album/20140718/201407181722411740.jpg',
+            'http://img4.xiazaizhijia.com/walls/20161013/mid_020251f05941d22.jpg',
+            'http://pic.bizhi360.com/bbpic/32/3032.jpg',
+         
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143910.jpg',
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143913.jpg',
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143915.jpg',
+
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143916.jpg',
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143917.jpg',
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143918.jpg',
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143919.jpg',
+            'http://www.dnzhuti.com/uploads/allimg/180629/95-1P629143920.jpg'
+        ];
         return (
             <HashRouter basename='manage' >
                 <main className='main' ref={this.main}>
-                    <aside>
-                        <PhotoSwipe 
+                    <aside style={{overflowY:'scroll'}}>
+                        {/* <PhotoSwipe 
                             isOpen={openPreview} 
                             items={previewList} 
                             options={{errorMsg:'图片加载失败，请检查网络',fullscreenEl: false}} 
                             imageLoadComplete={ this.imageLoadComplete.bind(this)}
                             onClose={this.handleClose.bind(this)}
-                        />
+                        /> */}
+
+
+            {
+                imgList.map((url,idx)=>(
+                    // <LazyLoad 
+                    // height={200} 
+                    // once={false}
+                    // key={idx} 
+                    // offset={[0, 100]}
+                    // overflow={true}
+                    // placeholder={<div style={{zIndex:'99999',color:'red'}}>图片加载中</div>}
+                    
+                    // >
+                    //     <img style={{width:'100%'}}src={url} /> 
+                    // </LazyLoad>
+                    <img 
+                    style={{width:'100%'}}
+                    onLoad={function(e){
+                        console.log(e,'图片加载中....')
+                    }}
+                    src={url}  key={idx}/> 
+                ))
+            }
+      
+
+      {/* <LazyLoad height={200}>
+        <img src="http://img.kuai8.com/attaches/album/20140718/201407181722411740.jpg" /> 
+      </LazyLoad> */}
+       
+                    <ImageLoader
+ src="https://cn.bing.com/th?id=OIP.vo8ZLtGBS_LGY8dKvTNCtgHaEo&pid=Api&rs=1"
+                        wrapper={React.createFactory('div')}
+                        preloader={preloader}>
+                        Image load failed!
+                    </ImageLoader>
+
+
+                   
                     </aside>
                 </main>
             </HashRouter>
